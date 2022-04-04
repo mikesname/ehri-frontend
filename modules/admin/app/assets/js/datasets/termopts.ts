b71be2d808c55402a0b3da151cf55324eca6b938
Mike Bryant <mikesname@gmail.com>
2022-04-04 09:46:38 +0100

    Tweak font styles for Xterm js

--- modules/admin/app/assets/js/datasets/termopts.ts
+++ modules/admin/app/assets/js/datasets/termopts.ts
@@ -1,9 +1,10 @@
-
 export default {
-  fontSize: 14,
-      theme: {
-    foreground: '#111',
+    fontSize: 14,
+    fontFamily: 'Roboto Mono, Ubuntu Mono, courier-new, courier, monospace',
+    lineHeight: 1.4,
+    theme: {
+        foreground: '#111',
         background: '#fff',
         selection: '#303030',
-  }
+    }
 }
